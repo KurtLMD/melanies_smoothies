56527f39399c8d2baeb5a197cd7c489b07aa8382
Kurt Drew <kurtd@acumenbi.co.nz>
2025-06-19 17:07:55 +1200

    Fix fruit options DataFrame display by including SEARCH_ON column and updating layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,10 @@ st.write(f"The name on your Smoothie will be: {name_on_order}")
 cnx = st.connection("snowflake")
 session = cnx.session()
 fruit_options_df = session.table("smoothies.public.fruit_options").select(
-    col("FRUIT_NAME")
+    col("FRUIT_NAME"), col("SEARCH_ON")
 )
+st.dataframe(data=fruit_options_df, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
